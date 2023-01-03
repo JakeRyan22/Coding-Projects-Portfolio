6a3c378853973c927e68e79cc610982db98eafe7
JakeRyan22 <114652326+JakeRyan22@users.noreply.github.com>
2023-01-02 19:35:45 -0500

    Delete displayAllBombs.m

--- displayAllBombs.m
+++
@@ -1,9 +0,0 @@
-for i = 1:boardSize
-    for j = 1:boardSize
-        if boardGen(i,j) == 9
-            pause(.05)
-            MineSweeperBoard{i,j} = mineSweeperGraphics{10};
-            dispGraphics;
-        end
-    end
-end
